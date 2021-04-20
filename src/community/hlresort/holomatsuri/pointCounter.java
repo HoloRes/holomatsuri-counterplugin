a5c14fb032a5bd898cdfa3ffb2c201b0ebaa7e8c
GoldElysium <goldelysium@gmail.com>
2021-04-20 10:47:22 +0200

    A base something

---
+++ src/community/hlresort/holomatsuri/pointCounter.java
@@ -0,0 +1,15 @@
+package community.hlresort.holomatsuri;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class pointCounter extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
