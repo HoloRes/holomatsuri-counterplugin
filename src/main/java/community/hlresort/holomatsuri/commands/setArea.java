6632269de872993bd38c7349d75af92cd5b2e80f
GoldElysium <goldelysium@gmail.com>
2021-04-22 15:18:09 +0200

    Add some config stuff
    HLMMCPL-1 #done
    Ignore the stupid Jira key, didn't have anything better

--- src/main/java/community/hlresort/holomatsuri/commands/setArea.java
+++ src/main/java/community/hlresort/holomatsuri/commands/setArea.java
@@ -1,13 +1,43 @@
 package community.hlresort.holomatsuri.commands;
 
+import community.hlresort.holomatsuri.pointCounter;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class setArea implements CommandExecutor {
+    Integer xStart;
+    Integer zStart;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Command executed");
+        if(!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be executed by a player!");
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if(xStart != null && zStart != null) {
+            pointCounter.config.set("coordinates.x.start", xStart);
+            pointCounter.config.set("coordinates.z.start", zStart);
+            pointCounter.config.set("coordinates.x.end", player.getLocation().getBlockX());
+            pointCounter.config.set("coordinates.z.end", player.getLocation().getBlockZ());
+            pointCounter.plugin.saveConfig();
+
+            sender.sendMessage("Start location: " + ChatColor.AQUA + xStart + ", " + zStart + ChatColor.WHITE + "\n" + "End location: " + ChatColor.AQUA + player.getLocation().getBlockX() + ", " + player.getLocation().getBlockZ());
+
+            xStart = null;
+            zStart = null;
+        } else {
+            xStart = player.getLocation().getBlockX();
+            zStart = player.getLocation().getBlockZ();
+
+            sender.sendMessage("Start location: " + ChatColor.AQUA + xStart + ", " + zStart);
+        }
+
         return true;
     }
 }
--- src/main/java/community/hlresort/holomatsuri/pointCounter.java
+++ src/main/java/community/hlresort/holomatsuri/pointCounter.java
@@ -1,16 +1,51 @@
 package community.hlresort.holomatsuri;
 
+import community.hlresort.holomatsuri.commands.setArea;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class pointCounter extends JavaPlugin {
+public class pointCounter extends JavaPlugin implements Listener {
+    public static pointCounter plugin;
+    public static FileConfiguration config;
+
     @Override
     public void onEnable() {
+        plugin = this;
+        config = this.getConfig();
+
         saveDefaultConfig();
+        connectToDb();
+
+        this.getCommand("setarea").setExecutor(new setArea());
+
         getLogger().info("Plugin loaded!");
     }
 
     @Override
     public void onDisable() {
+        plugin = null;
+    }
 
+    public void connectToDb() {
+        Connection conn = null;
+        try {
+            this.getDataFolder().mkdirs();
+            String url = "jdbc:sqlite:" + this.getDataFolder().getAbsolutePath() + "/data.db";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
